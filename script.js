fe80e1de9d971087b1f31a3fa350b96f33fcae7b
Gabriel Cury <166022990+gabrielcury30@users.noreply.github.com>
2025-04-28 15:19:12 -0300

    Adicionar funcao 'jogarJogo' para jogar quantas vezes queira e reestruturacao da logica do jogo

--- script.js
+++ script.js
@@ -1,63 +1,85 @@
-// 1 - Fazer a escolha aleatoria do computador entre pedra, papel e tesoura
-const valorEscolhidoComputador = () => {
-    const valorAleatorio = Math.floor((Math.random() * 3)) + 1
-    
-    if (valorAleatorio === 1) {
-        return "Pedra"
-    } else if (valorAleatorio === 2) {
-        return "Papel"
-    } else {
-        return "Tesoura"
-    }
-}
-
-// 2 - Fazer o usuário escolher entre pedra papel e tesoura
-const valorEscolhidoUsuario = () => {
-    const valorUsuario = prompt("Faça a escolha entre Pedra, Papel, ou, Tesoura!")
-    return valorUsuario
-}
-
-// 3 - Inicializar variáveis das pontuações do computador e usuário
+// Inicializar variáveis das pontuações do computador e usuário
 let pontuacaoUsuario = 0
 let pontuacaoComputador = 0
 
-// 4 - Lógica para a rodada do jogo
-const parametroValorComputador = valorEscolhidoComputador()
-const parametroValorUsuario = valorEscolhidoUsuario()
+console.log("Bem-vindo ao jogo pedra, papel e tesoura! Será uma melhor de 5. Divirta-se!")
+console.log("\n")
 
-const jogarRodada = (parametroValorComputador, parametroValorUsuario) => {
-    let valorComputadorPadronizado = parametroValorComputador.toLowerCase()
-    let valorUsuarioPadronizado = parametroValorUsuario.toLowerCase()
-
-    // console.log(valorComputadorPadronizado)
-    // console.log(valorUsuarioPadronizado)
-
-    if (valorComputadorPadronizado === "pedra" && valorUsuarioPadronizado === "tesoura") {
-        console.log("Você perdeu! Pedra ganha de tesoura!")
-        pontuacaoComputador++        
-    } else if (valorComputadorPadronizado === "pedra" && valorUsuarioPadronizado === "papel") {
-        console.log("Você venceu! Papel ganha de pedra!")
-        pontuacaoUsuario++
-    } else if (valorComputadorPadronizado === "tesoura" && valorUsuarioPadronizado === "pedra") {
-        console.log("Você venceu! Pedra ganha de tesoura!")
-        pontuacaoUsuario++
-    } else if (valorComputadorPadronizado === "tesoura" && valorUsuarioPadronizado === "papel") {
-        console.log("Você perdeu! Tesoura ganha de papel!")
-        pontuacaoComputador++
-    } else if (valorComputadorPadronizado === "papel" && valorUsuarioPadronizado === "pedra") {
-        console.log("Você perdeu! Papel ganha de pedra!")
-        pontuacaoComputador++        
-    } else if (valorComputadorPadronizado === "papel" && valorUsuarioPadronizado === "tesoura") {
-        console.log("Você venceu! Tesoura ganha de papel!")
-        pontuacaoUsuario++        
-    } else {
-        console.log("Os dois colocaram as mesmas coisas! Houve um empate!")
+// Função que envolve toda a lógica do jogo
+const jogarJogo = () => {
+    // Fazer a escolha aleatoria do computador entre pedra, papel e tesoura
+    const valorEscolhidoComputador = () => {
+        const valorAleatorio = Math.floor((Math.random() * 3)) + 1
+        
+        if (valorAleatorio === 1) {
+            return "Pedra"
+        } else if (valorAleatorio === 2) {
+            return "Papel"
+        } else {
+            return "Tesoura"
+        }
     }
+
+    // Fazer o usuário escolher entre pedra papel e tesoura
+    const valorEscolhidoUsuario = () => {
+        const valorUsuario = prompt("Faça a escolha entre Pedra, Papel, ou, Tesoura!")
+        return valorUsuario
+    }
+
+    // Lógica para a rodada do jogo
+    const parametroValorComputador = valorEscolhidoComputador()
+    const parametroValorUsuario = valorEscolhidoUsuario()
+
+    const jogarRodada = (parametroValorComputador, parametroValorUsuario) => {
+        let valorComputadorPadronizado = parametroValorComputador.toLowerCase()
+        let valorUsuarioPadronizado = parametroValorUsuario.toLowerCase()
+
+        console.log(`Computador: ${valorComputadorPadronizado}`)
+        console.log(`Você: ${valorUsuarioPadronizado}`)
+
+        if (valorComputadorPadronizado === "pedra" && valorUsuarioPadronizado === "tesoura") {
+            pontuacaoComputador++
+            console.log("Rodada perdida! Tesoura perde para pedra!")        
+        } else if (valorComputadorPadronizado === "pedra" && valorUsuarioPadronizado === "papel") {
+            pontuacaoUsuario++
+            console.log("Rodada ganha! Papel ganha de pedra!")
+        } else if (valorComputadorPadronizado === "tesoura" && valorUsuarioPadronizado === "pedra") {
+            pontuacaoUsuario++
+            console.log("Rodada ganha! Pedra ganha de tesoura!")
+        } else if (valorComputadorPadronizado === "tesoura" && valorUsuarioPadronizado === "papel") {
+            pontuacaoComputador++
+            console.log("Rodada perdida! Papel perde para tesoura!")
+        } else if (valorComputadorPadronizado === "papel" && valorUsuarioPadronizado === "pedra") {
+            pontuacaoComputador++
+            console.log("Rodada perdida! Pedra perde para papel!")        
+        } else if (valorComputadorPadronizado === "papel" && valorUsuarioPadronizado === "tesoura") {
+            pontuacaoUsuario++
+            console.log("Rodada ganha! Tesoura ganha de papel!")        
+        } else if (valorComputadorPadronizado === valorUsuarioPadronizado) {
+            console.log("Rodada empatada! Ambas escolhas se anulam!")
+        }
+    }
+
+    jogarRodada(parametroValorComputador, parametroValorUsuario)
+    console.log("\n")
 }
 
-// jogarRodada(parametroValorComputador, parametroValorUsuario)
-// console.log(pontuacaoComputador)
-// console.log(pontuacaoUsuario)
+// Chamar a função do jogo 5 vezes para ser uma melhor de 5 rodadas
+jogarJogo()
+jogarJogo()
+jogarJogo()
+jogarJogo()
+jogarJogo()
 
-// 4 - Regras: pedra > tesoura, tesoura > papel, papel > pedra
-// 5 - Imprimir o vencedor
+// Impressão das pontuações e vencedor
+console.log(`Pontuação do computador: ${pontuacaoComputador}`)
+console.log(`Sua pontuação: ${pontuacaoUsuario}`)
+console.log("\n")
+
+if (pontuacaoComputador > pontuacaoUsuario) {
+    console.log("Que pena! Infelizmente você perdeu o jogo!")    
+} else if (pontuacaoComputador < pontuacaoUsuario) {
+    console.log("Parabéns! Você é o grande vencedor!")
+} else {
+    console.log("Que luta! A partida se configurou em um empate!")
+}
