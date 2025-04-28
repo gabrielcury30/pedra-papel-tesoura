08c1a9df88b2da839d0312f571272aa2284a6deb
Gabriel Cury <166022990+gabrielcury30@users.noreply.github.com>
2025-04-28 14:14:24 -0300

    Adicionar logica de rodada no jogo

--- script.js
+++ script.js
@@ -17,6 +17,47 @@ const valorEscolhidoUsuario = () => {
     return valorUsuario
 }
 
-// 3 - Comparar as escolhas do usuário com a do computador para ver o vencedor
+// 3 - Inicializar variáveis das pontuações do computador e usuário
+let pontuacaoUsuario = 0
+let pontuacaoComputador = 0
+
+// 4 - Lógica para a rodada do jogo
+const parametroValorComputador = valorEscolhidoComputador()
+const parametroValorUsuario = valorEscolhidoUsuario()
+
+const jogarRodada = (parametroValorComputador, parametroValorUsuario) => {
+    let valorComputadorPadronizado = parametroValorComputador.toLowerCase()
+    let valorUsuarioPadronizado = parametroValorUsuario.toLowerCase()
+
+    // console.log(valorComputadorPadronizado)
+    // console.log(valorUsuarioPadronizado)
+
+    if (valorComputadorPadronizado === "pedra" && valorUsuarioPadronizado === "tesoura") {
+        console.log("Você perdeu! Pedra ganha de tesoura!")
+        pontuacaoComputador++        
+    } else if (valorComputadorPadronizado === "pedra" && valorUsuarioPadronizado === "papel") {
+        console.log("Você venceu! Papel ganha de pedra!")
+        pontuacaoUsuario++
+    } else if (valorComputadorPadronizado === "tesoura" && valorUsuarioPadronizado === "pedra") {
+        console.log("Você venceu! Pedra ganha de tesoura!")
+        pontuacaoUsuario++
+    } else if (valorComputadorPadronizado === "tesoura" && valorUsuarioPadronizado === "papel") {
+        console.log("Você perdeu! Tesoura ganha de papel!")
+        pontuacaoComputador++
+    } else if (valorComputadorPadronizado === "papel" && valorUsuarioPadronizado === "pedra") {
+        console.log("Você perdeu! Papel ganha de pedra!")
+        pontuacaoComputador++        
+    } else if (valorComputadorPadronizado === "papel" && valorUsuarioPadronizado === "tesoura") {
+        console.log("Você venceu! Tesoura ganha de papel!")
+        pontuacaoUsuario++        
+    } else {
+        console.log("Os dois colocaram as mesmas coisas! Houve um empate!")
+    }
+}
+
+// jogarRodada(parametroValorComputador, parametroValorUsuario)
+// console.log(pontuacaoComputador)
+// console.log(pontuacaoUsuario)
+
 // 4 - Regras: pedra > tesoura, tesoura > papel, papel > pedra
 // 5 - Imprimir o vencedor
